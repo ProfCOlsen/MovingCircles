34987c96ffcb47f9d49e39a64b52b63c1343eecf
Christopher Olsen <clolsen2@asurite.asu.edu>
2017-11-20 06:01:15 -0700

    Removed a from comment

--- MovingCircles/Form1.cs
+++ MovingCircles/Form1.cs
@@ -18,7 +18,7 @@ namespace MovingCircles
         int circleY = 0;
         int lastX = 300;
         int lastY = 0;
-        //Add a line from ProfCOlsen
+        //Add line from ProfCOlsen
         string name = "Chris";
 
 
